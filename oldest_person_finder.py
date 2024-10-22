ee4fafda3e752530391cf962fc23156d0a9d9cdc
unknown <diamantecarl9@gmail.com>
2024-10-23 03:20:30 +0800

    working def

--- oldest_person_finder.py
+++ oldest_person_finder.py
@@ -1,5 +1,7 @@
 
 # Define a valid name (The name must only contain letters, spaces, and period for middle initials and shortened junior and senior)
+def valid_name_criteria(name):
+    return name.replace(" ", "").isalpha()
 # Define a valid age criteria (The age must be a positive integer between 0 and 122 - the highest age is based on Jeanne Calment, a French woman who passed away at the age of 122 years old in 1997)
 # Find the person with the highest age
 # Set up an array to store people's information
